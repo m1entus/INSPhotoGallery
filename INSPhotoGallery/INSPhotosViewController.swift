f16e0017630eee6ab7ae1ad10f9c377ee76e6898
danwey <509070768@qq.com>
2016-05-12 11:38:53 +0800

    Update INSPhotosViewController.swift
    public datasource

--- INSPhotoGallery/INSPhotosViewController.swift
+++ INSPhotoGallery/INSPhotosViewController.swift
@@ -83,7 +83,7 @@ public class INSPhotosViewController: UIViewController, UIPageViewControllerData
     
     // MARK: - Private
     private(set) var pageViewController: UIPageViewController!
-    var dataSource: INSPhotosDataSource
+    public var dataSource: INSPhotosDataSource
     
     let interactiveAnimator: INSPhotosInteractionAnimator = INSPhotosInteractionAnimator()
     let transitionAnimator: INSPhotosTransitionAnimator = INSPhotosTransitionAnimator()
