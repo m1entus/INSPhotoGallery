6da9e3feba97826db9f72bc55b2424c962652015
Effort <effort@ErikWork.local>
2019-05-03 15:06:36 -0500

    update shareButton

--- INSPhotoGallery/INSPhotosOverlayView.swift
+++ INSPhotoGallery/INSPhotosOverlayView.swift
@@ -52,7 +52,8 @@ open class INSPhotosOverlayView: UIView , INSPhotosOverlayViewable {
     }
     open var rightBarButtonItem: UIBarButtonItem? {
         didSet {
-            navigationItem.rightBarButtonItem = rightBarButtonItem
+            //navigationItem.rightBarButtonItem = rightBarButtonItem
+            navigationItem.rightBarButtonItem = nil
         }
     }
     
